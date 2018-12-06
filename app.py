e2f553a07725e54ca28ed348edde56e386a46158
LI0131 <mccannliam.lm@gmail.com>
2018-12-06 16:14:23 -0500

    Fixed indentation error

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 """
- Author: Liam McCann
- file: app.py
- This file runs the webapp for Heroku
- Created: December 6, 2018
+Author: Liam McCann
+file: app.py
+This file runs the app in heroku
+Created: December 6, 2018
 """
-
 def main():
-    #This is a blank app template
+    #This is a template for the app
+    print("Hello Heroku!")
 
 if __name__ == "__main__":
     main()
