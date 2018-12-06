2fb09474a40e3f4ca6b2c30027b4c1a39a9096e6
LI0131 <mccannliam.lm@gmail.com>
2018-12-06 16:08:05 -0500

    fixed error in app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-# Author: Liam McCann
-# file: app.py
-# This file runs the webapp for Heroku
-# Created: December 6, 2018
+"""
+ Author: Liam McCann
+ file: app.py
+ This file runs the webapp for Heroku
+ Created: December 6, 2018
+"""
 
 def main():
     #This is a blank app template
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
